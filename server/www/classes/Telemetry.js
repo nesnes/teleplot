fb599283e5b17274a762f22fd60e3bbd1a30e73d
maximilien.charmetant <maximilien.charmetant@wandercraft.eu>
2022-09-29 17:33:49 +0200

    first version

--- server/www/classes/Telemetry.js
+++ server/www/classes/Telemetry.js
@@ -1,23 +1,39 @@
 /*Telemetries can be of two types: 
  
+<<<<<<< HEAD
 - "number"
     in this case, data, pendingData, values and xy are used and textFormatValue is undefined
 
 - "text"
+=======
+- "default"
+    in this case, data, pendingData, values and xy are used and textFormatValue is undefined
+
+- "textBased"
+>>>>>>> first version
     in this case, data, pendingData, values and xy are not used and textFormatValue is used
   
  */
 
 
 class Telemetry{
+<<<<<<< HEAD
     constructor(_name, isTimeBased, unit = undefined, type = "number"){
         this.type = type; // either "number" or "text"
+=======
+    constructor(_name, isTimeBased, unit = undefined, type = "default"){
+        this.type = type; // either "default" or "textBased"
+>>>>>>> first version
         this.name = _name;
         this.unit = ( unit != "" ) ? unit : undefined;
         this.usageCount = 0;
 
         
+<<<<<<< HEAD
         // an array of Number or String containing the last value recieved of the telemetry
+=======
+        // an array of Number containing the last value recieved of the telemetry
+>>>>>>> first version
         // contains either one (if !xy) or two values (if xy).
         this.values = [];
 
@@ -25,11 +41,19 @@ class Telemetry{
         this.data = [[],[]]; // data[0] contains the timestamps and data[1] contains the values corresponding to each timestamp
         this.pendingData = [[],[]];
         
+<<<<<<< HEAD
+=======
+        // type : String, if telemetry is of type "textFormatted", contains the last received value
+        this.textFormatValue = undefined;
+        
+
+>>>>>>> first version
         if(!isTimeBased){
             this.data.push([]);
             this.pendingData.push([]);
         }
 
+<<<<<<< HEAD
     }
 
     getValuesFormatted() {
@@ -50,14 +74,25 @@ class Telemetry{
 
         return "";
        
+=======
+>>>>>>> first version
     }
 
     getValuesFormatted() {
-        let res = this.values[0].toFixed(4);
-        
-        if (this.xy && this.values.length == 2)
-            res += ("  " + this.values[1].toFixed(4));
 
-        return res;
+        if (this.type == "default")
+        {
+            let res = this.values[0].toFixed(4);
+        
+            if (this.xy && this.values.length == 2)
+                res += ("  " + this.values[1].toFixed(4));
+    
+            return res;
+        }
+        else if (this.type == "textBased")
+        {
+            return this.textFormatValue;
+        }
+       
     }
 }
