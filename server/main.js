5138b217fd6318366c3a3c7a6aa0385ba8776262
Alexandre Brehmer <alex-nesnes@hotmail.fr>
2024-10-24 13:39:04 +0200

    Add err handler and better logs

--- server/main.js
+++ server/main.js
@@ -3,6 +3,13 @@ const CMD_UDP_PORT = 47268;
 const HTTP_WS_PORT = 80;
 const requestDelay = 50;// every requestDelay milliseconds, we send a websocket packet 
 
+console.original_log = console.log.bind(console);
+console.original_error = console.error.bind(console);
+console.original_warn = console.warn.bind(console);
+console.log =   function(){ console.original_log.apply(  console, ["I", new Date().toISOString(), "|"].concat( [].slice.call(arguments) ))};
+console.error = function(){ console.original_error.apply(console, ["E", new Date().toISOString(), "|"].concat( [].slice.call(arguments) ))};
+console.warn =  function(){ console.original_warn.apply( console, ["W", new Date().toISOString(), "|"].concat( [].slice.call(arguments) ))};
+
 const fs = require('fs');
 var http = require('http');
 var https = require('https');
@@ -43,13 +50,15 @@ let clientList = {};
 let onWebSocket = (ws, req)=>{
     // Parse port
     let udpPort = Number.parseInt(req.params.port);
-    if(udpPort<1024 || udpPort>65535){
+    if(udpPort<1024 || udpPort>65535 || !Number.isFinite(udpPort)){
         ws.close();
         return;
     }
-
+    
     // Add in client list
     if(clientList[udpPort] == undefined) {
+        let remoteIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        console.log("WS connection on", req.params.port, "from", remoteIp);
         clientList[udpPort] = {
             udpPort: udpPort,
             ws: ws,
@@ -74,10 +83,15 @@ let onWebSocket = (ws, req)=>{
 
         ws.on('close', (code, reason)=>{
             if(clientList[udpPort]){
+                console.log("WS disconnection on", udpPort);
                 clearInterval(clientList[udpPort].interval);
                 clientList[udpPort].udp.close();
                 clientList[udpPort] = undefined;
             }
+        });
+
+        ws.on('error', (error) => {
+            console.error("Websocket error:", error);
         });
     }
     else {
